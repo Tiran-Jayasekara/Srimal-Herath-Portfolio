4921a75991b7a86fa402003468e3ec0692d9b8aa
Tiran Jayasekara <70306643+Tiran-Jayasekara@users.noreply.github.com>
2026-03-28 18:13:13 +0530

    Fix
    Fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: '/Tiran-Jayasekara-Portfolio',
+  assetPrefix: '/Tiran-Jayasekara-Portfolio/',
 
   images: {
     unoptimized: true,
